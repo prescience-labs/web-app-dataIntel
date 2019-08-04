a16cf938bd34bd55dd5df2fcba1029d58d7bbfd3
CorrOrtiz <a.ortizcrr@gmail.com>
2019-08-04 02:00:05 -0500

    WIP landing

--- src/Components/LandingPage/LandingPageContainer.js
+++ src/Components/LandingPage/LandingPageContainer.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import LandingPageDisplay from './LandingPageDisplay';
 
 function LandingPageContainer() {
-  return <LandingPageDisplay />;
+  const [openDialog, setOpenDialog] = useState(false);
+
+  function handleClickOpen() {
+    setOpenDialog(true);
+  }
+
+  function handleClose() {
+    setOpenDialog(false);
+  }
+
+  return (
+    <LandingPageDisplay
+      openDialog={openDialog}
+      handleClickOpen={handleClickOpen}
+      handleClose={handleClose}
+    />
+  );
 }
 
 export default LandingPageContainer;
---
+++ src/Components/LandingPage/LandingPageStyle.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/styles';
+
+export const landingPageStyle = makeStyles({
+  dialogStyle: {
+    height: '20rem',
+  },
+  hederStyle: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    padding: '1rem 5rem',
+    marginBottom: '10rem',
+  },
+});
