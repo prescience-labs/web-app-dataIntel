b23aaf7d484f45d8b60886cbcba0f98ea4461b46
CorrOrtiz <a.ortizcrr@gmail.com>
2019-08-04 02:00:05 -0500

    WIP landing

---
+++ src/Components/LandingPage/LandingPageStyle.js
@@ -0,0 +1,11 @@
+export const landingPageStyle = theme => ({
+  rootStyle: {
+    minHeight: '450px',
+    padding: '30px 40px',
+    borderRadius: '15px',
+    backgroundColor: '#ffffff',
+    boxShadow: '0 9px 16px 0 rgba(224, 229, 238, .5)',
+    border: 'solid 1px #e5eaed',
+    position: 'relative',
+  },
+});
