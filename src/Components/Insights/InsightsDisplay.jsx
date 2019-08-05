845dd0f7dd68160a0ec4757f62e5f388feadc625
CorrOrtiz <a.ortizcrr@gmail.com>
2019-08-05 02:17:30 -0500

    adding result list

--- src/Components/Insights/InsightsDisplay.jsx
+++ src/Components/Insights/InsightsDisplay.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+
+import ResultsList from './ResultsList';
+
+const mockInsight = ['boots are AWSOME', 'best purchese ever'];
 
 function InsightsDisplay() {
-  return <h1>Insights</h1>;
+  return (
+    <Grid container justify="center">
+      <Grid item>
+        <ResultsList title="Posite" results={mockInsight} />
+      </Grid>
+      <Grid item>
+        <ResultsList title="Negative" results={mockInsight} />
+      </Grid>
+    </Grid>
+  );
 }
 
 export default InsightsDisplay;
