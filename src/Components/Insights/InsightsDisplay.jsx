95563782b901e722ee356293256484b3abb80126
Spencer Stolworthy <sdstolworthy@gmail.com>
2019-08-06 08:54:57 -0600

    added paper to charts and table in insights

--- src/Components/Insights/InsightsDisplay.jsx
+++ src/Components/Insights/InsightsDisplay.jsx
@@ -1,25 +1,37 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-
+import Paper from '@material-ui/core/Paper';
 import ResultsList from './ResultsList';
 import Chart from './Chart';
+import { makeStyles } from '@material-ui/core/styles';
 
 const mockInsight = ['boots are AWSOME', 'best purchese ever'];
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(1, 0),
+    margin: theme.spacing(4, 1),
+  },
+}));
 
 function InsightsDisplay() {
+  const { root } = useStyles();
   return (
     <Grid container justify="center">
-      <Grid item>
-        <Chart />
-      </Grid>
-      <Grid container justify="center">
+      <Paper className={root}>
         <Grid item>
-          <ResultsList title="Posite" results={mockInsight} />
+          <Chart />
         </Grid>
-        <Grid item>
-          <ResultsList title="Negative" results={mockInsight} />
+      </Paper>
+      <Paper className={root}>
+        <Grid container justify="center">
+          <Grid item>
+            <ResultsList title="Posite" results={mockInsight} />
+          </Grid>
+          <Grid item>
+            <ResultsList title="Negative" results={mockInsight} />
+          </Grid>
         </Grid>
-      </Grid>
+      </Paper>
     </Grid>
   );
 }
