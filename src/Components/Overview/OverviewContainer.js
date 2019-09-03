7794cba4db764732ced07f9aa47ce5ca02fb94c7
CorrOrtiz <a.ortizcrr@gmail.com>
2019-09-02 19:49:49 -0500

    WIP Auth

--- src/Components/Overview/OverviewContainer.js
+++ src/Components/Overview/OverviewContainer.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import useReactRouter from 'use-react-router';
 
 import OverviewDisplay from './OverviewDisplay';
 
 function OverviewContainer() {
+  const { match } = useReactRouter();
+  const { token, id } = match.params;
+
+  if (token && token !== 'false') {
+  }
+
+  if (id) {
+  }
+
   return <OverviewDisplay />;
 }
 
